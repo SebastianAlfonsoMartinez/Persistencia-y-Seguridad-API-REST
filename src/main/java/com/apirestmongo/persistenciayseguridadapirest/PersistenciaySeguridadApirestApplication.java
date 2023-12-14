8d651273947d7f3a400c4447f2d5406e32b0e9f0
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2023-12-14 16:10:58 -0500

    Creacion de proyecto y de carpetas estruturadas

---
+++ src/main/java/com/apirestmongo/persistenciayseguridadapirest/PersistenciaySeguridadApirestApplication.java
@@ -0,0 +1,13 @@
+package com.apirestmongo.persistenciayseguridadapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaySeguridadApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaySeguridadApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirestmongo/persistenciayseguridadapirest/PersistenciaySeguridadApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestmongo.persistenciayseguridadapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaySeguridadApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
